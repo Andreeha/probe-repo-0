bc849bda0eedd5c5e58f01050b6303b469925487
Andreeha <ofpprts@mail.ru>
2023-09-25 12:00:13 +0300

    merged

--- probe.c
+++ probe.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 // Creating new branch
 
-<<<<<<< HEAD
 int main () {
 
   return 0;
 }
-=======
+
 int conflict() { return -1; }
->>>>>>> 7a0e349ebf2c4e2549ae90235e69fd1422b20c1d
